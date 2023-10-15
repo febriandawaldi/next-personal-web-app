c9e74b05c6cce872d03b207ef9b6600c87fa0e7e
waldi <febrianda.waldi@gmail.com>
2023-10-15 21:15:42 +0700

    contact

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -26,19 +26,19 @@ const ContactPage = () => {
           <div className="mb-8">
             <h2 className="mb-3 text-lg font-bold">STREET ADDRESS</h2>
             <p className="text-sm leading-5">
-              Royal Sentraland BTP Cluster Sunderland E05/01. <br />
-              Maros, Sulawesi Selatan.
+              Kebayoran lama, Jakarta Selatan <br />
+              Jakarta Selatan.
             </p>
           </div>
           <div className="mb-8">
             <h2 className="mb-3 text-lg font-bold">EMAIL ADDRESS</h2>
             <p className="text-sm leading-5">
               <a href="mailto:me@dedeard.my.id" rel="noopener" className="hover:text-yellow-600">
-                me@dedeard.my.id
+                febrianda.waldi@gmail.com
               </a>
               <br />
               <a href="mailto:dedeariansya1@gmail.com" rel="noopener" className="hover:text-yellow-600">
-                dedeariansya1@gmail.com
+                febrianda.waldi@gmail.com
               </a>
             </p>
           </div>
@@ -47,12 +47,12 @@ const ContactPage = () => {
             <p className="text-sm leading-5">
               <span>Call: </span>
               <a href="tel:+6281343912883" target="_blank" rel="noopener" className="hover:text-yellow-600">
-                +62 813-4391-2883
+                +62 822-2616-6717
               </a>
               <br />
               <span>WhatsApp: </span>
               <a href={SOCIALS.WA} target="_blank" rel="noopener" className="hover:text-yellow-600">
-                +62 813-4391-2883
+                +62 822-2616-6717
               </a>
             </p>
           </div>
