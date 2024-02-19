561c93ae0282cd88706c61e0431fcc6e6fc7a836
waldi <febrianda.waldi@gmail.com>
2024-02-19 10:45:26 +0700

    update email

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -34,7 +34,7 @@ const ContactPage = () => {
             <h2 className="mb-3 text-lg font-bold">EMAIL ADDRESS</h2>
             <p className="text-sm leading-5">
               <a href="mailto:me@dedeard.my.id" rel="noopener" className="hover:text-yellow-600">
-                febrianda.waldi@gmail.com
+                fattoofittoo@gmail.com
               </a>
               <br />
               <a href="mailto:dedeariansya1@gmail.com" rel="noopener" className="hover:text-yellow-600">
